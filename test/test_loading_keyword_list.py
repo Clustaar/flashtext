b9d06b4bdedba0237a8526a56aee6ec0b5ef5915
Amin Messaoudi <amin.messaoudi@clustaar.com>
2019-11-20 11:40:45 +0100

    update some tests

--- test/test_loading_keyword_list.py
+++ test/test_loading_keyword_list.py
@@ -25,5 +25,17 @@ class TestListLoad(unittest.TestCase):
         self.assertEqual(sentence_new, "I know java and product management",
                          "Failed file format one test")
 
+    def test_list_loading_first_occ(self):
+        keyword_processor = KeywordProcessor()
+        keyword_list = ["java", "product management"]
+        keyword_processor.add_keywords_from_list(keyword_list)
+        sentence = 'I know java and product management'
+        keywords_extracted = keyword_processor.extract_keywords(sentence, stop_at_first_occ=True)
+        self.assertEqual(keywords_extracted, ['java'],
+                         "Failed file format one test")
+        sentence_new = keyword_processor.replace_keywords(sentence)
+        self.assertEqual(sentence_new, "I know java and product management",
+                         "Failed file format one test")
+
 if __name__ == '__main__':
     unittest.main()
